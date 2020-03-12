d17347060bb51b2a58f1077a428ff54253d56fd8
Parables Boltnoel <parables95@gmail.com>
2020-03-12 18:42:11 +0000

    Starter template complete

--- src/App.svelte
+++ src/App.svelte
@@ -1,26 +1,11 @@
 <script>
-  let framework = 'svelte.js';
-  let bundler = 'parcel.js';
-  let css = 'tailwind.css';
+  
 </script>
 
-<div class="font-inter flex flex-grow h-screen justify-center items-center">
+<div class="flex items-center justify-center flex-grow h-screen font-inter">
   <div class="text-center">
     <h1 class="text-5xl">
-      <span class="text-yellow-900">{framework}</span>
-      +
-      <span class="text-blue-900">{css}</span>
-      +
-      <span class="text-green-900">{bundler}</span>
-      =
-      <span class="font-semibold text-red-900">awesome</span>
-    </h1>
-    <p class="text-3xl mt-4">
-      * also includes
-      <strong>Prettier</strong>
-      and
-      <strong>ESLint</strong>
-      configs with opinionated settings
-    </p>
+<strong>Let's start building your app right away </strong>
+     </h1>
   </div>
 </div>
