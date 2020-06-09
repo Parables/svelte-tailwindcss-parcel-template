cc3ad86ff7659e5ffe6929ea7c3ec05e449abacd
Parables Boltnoel <parables95@gmail.com>
2020-06-09 06:26:41 +0000

    Added typescript suport

---
+++ src/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  const a: string = "Svelte",
+    b: string = "Typescript",
+    c: string = "Parcel";
+</script>
+
+<h1>{a} + {b} + {c}</h1>
+<button class="bg-blue-400 px-4 py-2 rounded-md">Hello</button>
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import Svelte from './index.svelte'
+
+const app =new Svelte({
+  target: document.body
+})
+
+export default app;
